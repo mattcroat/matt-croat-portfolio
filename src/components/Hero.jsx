0da786194155afb58977b11329b638f121114f29
mattcroat <mattcroat.dev@gmail.com>
2020-02-03 17:46:34 +0100

    Fix projects animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -28,32 +28,38 @@ function Hero() {
           <h2>Latest Projects — <a href="https://github.com/mattcroat/">GitHub</a></h2>
 
           <CardsStyle>
-            <a
-              className="project-1"
-              href="https://sick-kicks.netlify.com"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <h3>Sick Kicks · shop</h3>
-            </a>
+            <div className="project-card">
+              <a
+                className="project-1"
+                href="https://sick-kicks.netlify.com"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <h3>Sick Kicks · shop</h3>
+              </a>
+            </div>
 
-            <a
-              className="project-2"
-              href="https://amorebistro.netlify.com"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <h3>Bistro · restaurant</h3>
-            </a>
+            <div className="project-card">
+              <a
+                className="project-2"
+                href="https://amorebistro.netlify.com"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <h3>Bistro · restaurant</h3>
+              </a>
+            </div>
 
-            <a
-              className="project-3"
-              href="https://surfsup.netlify.com"
-              target="_blank"
-              rel="noreferrer noopener"
-            >
-              <h3>Surf · landing page</h3>
-            </a>
+            <div className="project-card">
+              <a
+                className="project-3"
+                href="https://surfsup.netlify.com"
+                target="_blank"
+                rel="noreferrer noopener"
+              >
+                <h3>Surf · landing page</h3>
+              </a>
+            </div>
           </CardsStyle>
         </ProjectsStyle>
       </section>
@@ -118,55 +124,72 @@ const ProjectsStyle = styled.div`
 `;
 
 const CardsStyle = styled.div`
-  margin-top: 1rem;
+  margin-top: 1.4rem;
   display: flex;
   justify-content: center;
-  /* transform: translateY(50%); */
-	/* animation: var(--shortFade), slide-in 1s forwards; */
 
   @media (max-width: 1000px) {
     flex-direction: column;
     align-items: center;
   }
 
+  .project-card {
+    transition: transform 0.4s;
+    margin: 1rem;
+
+    @media (max-width: 1000px) {
+      width: 70%;
+
+      a {
+        width: 100%;
+        background-position: top center;
+      }
+    }
+
+    @media (max-width: 600px) {
+      width: 90%;
+    }
+
+    &:hover {
+      transform: scale(1.4);
+      z-index: 3;
+
+      @media (max-width: 1000px) {
+        transform: scale(1.1);
+      }
+
+      @media (max-width: 600px) {
+        transform: scale(1.08);
+      }
+    }
+  }
+
   a {
     display: inline-block;
     width: 320px;
     height: 200px;
-    margin: 0.4rem;
     position: relative;
     /* background-image: url('https://images.unsplash.com/photo-1483728642387-6c3bdd6c93e5'); */
     background-size: cover;
     background-position: center center;
     border-radius: 2%;
-    transition: transform 0.4s;
     overflow: hidden;
-
     transform: translateY(50%);
     animation: var(--shortFade), slide-in 1s forwards;
 
-
     &.project-1 {
       background-image: url(${kicks});
-      animation-duration: 1s;
+      transform: translateX(-50%);
     }
     
     &.project-2 {
       background-image: url(${bistro});
-      animation-duration: 1.2s;
+      transform: translateY(50%);
     }
     
     &.project-3 {
       background-image: url(${surf});
-      animation-duration: 1.4s;
-    }
-
-    @media (max-width: 1000px) {
-      width: 70%;
-    }
-
-    @media (max-width: 600px) {
-      width: 90%;
+      transform: translateX(50%);
     }
 
     &::after {
@@ -180,15 +203,8 @@ const CardsStyle = styled.div`
       z-index: 1;
     }
 
-    &:hover {
-      cursor: pointer;
-      transform: scale(1.4);
+    &:hover::after {
       box-shadow: 0 4px 14px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
-      z-index: 3;
-
-      @media (max-width: 1000px) {
-        transform: scale(1.02);
-      }
 
       &::after {
         opacity: 0;
